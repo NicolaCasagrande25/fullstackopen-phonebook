ea37244da1f6365188720824dd427b9d1557b535
nicolac <nicola.casagrande2000@gmail.com>
2024-01-20 23:04:27 +0200

    Part3 ex11

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
     return [
