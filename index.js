06244e48a85b3e0b2db1a10a9b3c7d360969ee3f
nicolac <nicola.casagrande2000@gmail.com>
2024-01-24 15:08:33 +0200

    Part3 ex15

--- index.js
+++ index.js
@@ -65,10 +65,9 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    contacts = contacts.filter(contact => contact.id !== id)
+    Contact.findByIdAndDelete(request.params.id)
+        .then(result => { response.status(204).end() })
 
-    response.status(204).end()
 })
 
 app.post('/api/persons', (request, response) => {
