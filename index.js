c7eb84025c0f7be1128f3adff56440826191b52f
nicolac <nicola.casagrande2000@gmail.com>
2024-01-24 15:21:33 +0200

    Part3 ex16

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const morgan = require('morgan')
 const app = express()
 const Contact = require('./models/contact')
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
@@ -42,10 +56,10 @@ let contacts = [
     }
 ]
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Contact.find({}).then(contacts => {
         response.json(contacts)
-    })
+    }).catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -64,13 +78,13 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Contact.findByIdAndDelete(request.params.id)
         .then(result => { response.status(204).end() })
-
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const person = new Contact({
         name: request.body.name,
         number: request.body.number
@@ -94,8 +108,11 @@ app.post('/api/persons', (request, response) => {
 
     person.save().then(savedPerson => {
         response.status(201).json(savedPerson)
-    })
+    }).catch(error => next(error))
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
